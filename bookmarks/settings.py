de82252d75423a7c0615ab003011593724cc4ee7
ja <jaime.hernan95@gmail.com>
2018-07-26 23:31:11 +0900

    Update requirements

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -94,7 +94,7 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(
     conn_max_age=600,
-    default='sqlite3:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+    default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
     )
 }
 
