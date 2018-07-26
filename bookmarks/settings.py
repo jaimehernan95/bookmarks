d5524b619866631b35a09aacacf9488e005c4e97
ja <jaime.hernan95@gmail.com>
2018-07-27 02:04:34 +0900

    Update settings and requirements

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'vir_proj_dir', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
