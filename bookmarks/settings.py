b985a7109455607d96dfaa9235730d0751782db9
ja <jaime.hernan95@gmail.com>
2018-07-26 22:48:05 +0900

    Update settings and requirements

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -32,7 +32,10 @@ if DEBUG in ['Off', 'off', 'No', 'False', 'false', '0', '']:
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'murmuring-mountain-79062.herokuapp.com',
+    'localhost',
+]
 
 
 # Application definition
@@ -86,11 +89,12 @@ WSGI_APPLICATION = 'bookmarks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+    conn_max_age=600,
+    default='sqlite3:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+    )
 }
 
 
