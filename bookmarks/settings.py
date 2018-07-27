0d655550337840adcb097707973d5275f6b427d2
ja <jaime.hernan95@gmail.com>
2018-07-27 16:23:36 +0900

    Update settings and requirements

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+# Configure Django App for Heroku.
+
+import django_heroku
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
